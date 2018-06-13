2849f8dd046aaf163daacf07a38dfa8a6d504548
Anton Buyskikh <anton.buyskikh@gmail.com>
2018-06-13 10:47:17 +0100

    Ex2: finish Part 1

--- ex2/ex2_manual.py
+++ ex2/ex2_manual.py
@@ -36,7 +36,8 @@ def h(X,theta):
 
 
 
-def getCostAndGrad(X,y,theta):
+def getCostAndGrad(theta,X,y):
+    # Returns the cost function and its gradient Sec. 1.2.2
     m,n=X.shape
     J=np.mean(-y*np.log(h(X,theta))-(1-y)*(np.log(1-h(X,theta))))
     grad=(h(X,theta)-y)@X/m
@@ -45,10 +46,10 @@ def getCostAndGrad(X,y,theta):
 
 
 def predict(X,theta):
-    print("Doublecheck this one")
     return np.where(h(X,theta)>0.5,1,0)
 
-#%% load ex2data1.txt - logical regression with two parameters
+#%% PART I
+# load ex2data1.txt - logical regression with two parameters
     
 data1=pd.read_csv('data/ex2data1.txt',names=['exam1','exam2','score'])
 y=np.asarray(data1.score)
@@ -62,40 +63,45 @@ fig.show()
 
 #%% get const functions
 
-theta=np.zeros(3)
-cost,grad=getCostAndGrad(X,y,theta)
-print('Cost at initial theta (zeros): \n', cost)
-print('Gradient at initial theta (zeros): \n',grad)
+theta_init=np.zeros(3)
+cost,grad=getCostAndGrad(theta_init,X,y)
+print('Cost at theta_init=[0 0 0]: \n', cost)
+print('Gradient at theta_init=[0 0 0]: \n',grad, '\n')
 
-#%% solution via the Gradient Decent and Normal Equation
+#%% solution via the optimization algorithm
+
+res=scipy.optimize.minimize(getCostAndGrad,
+                            theta_init,
+                            args=(X,y),
+                            method='Newton-CG',
+                            jac=True,
+                            options={'maxiter':400,'disp':True})
+
+theta_opt=res.x
+print('theta_opt: ',theta_opt)
+print('Cost at theta_opt: ',res.fun, '\n')
+
+#%% plot the solution from the previous block
+
+#NB: there should be a nicer way!!!
+x1=np.linspace(30,100,61)
+x2=(-theta_opt[0]-theta_opt[1]*x1)/theta_opt[2]
+fig,ax=plotData(X[:,1:],y)
+ax.plot(x1,x2,'-r')
+ax.legend(['Admitted','Not admitted','Decision boundary'])
+fig.show()
+
+#%% prediction and accuracy
+
+print('For a student with scores (45,85), admission probability is',
+      h(np.array([1,45,85]),theta_opt))
+
+# Accuracy on the training set with the optimal parameters
+print('Train Accuracy: ',np.mean(predict(X,theta_opt)==y)*100, '\n')
+
+#%% PART II
 
 
 
-#%% plot the solution via the Gradient Decent
-
-
-
-#%% plot convergence of the Gradient Decent
-
-
-
-#%% plot the cost function
-
-
-
-#%% load dataset2
- 
-
-#%% normalize features
-
-
-#%% solution via the Gradient Decent and Normal Equation
-
-
-#%% plot convergence of the Gradient Decent
-
-
-
-#%% prediction
 
 
