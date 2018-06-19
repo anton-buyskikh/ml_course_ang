94ffa1b8c9668a9e0cb4eb46612c92bc9595f316
Anton Buyskikh <anton.buyskikh@gmail.com>
2018-06-19 16:47:55 +0100

    Ex4: fix cost function mismatch due to Matlab indexing

--- ex4/ex4_manual.py
+++ ex4/ex4_manual.py
@@ -24,16 +24,6 @@ def h(X,theta):
 
 
 
-def sigmoid(z):
-    return 1.0/(1+np.exp(-z))
-
-
-
-def sigmoidGradient(z):
-    return sigmoid(z)*(1-sigmoid(z))
-
-
-
 def getCostReg(theta,X,y,lam_par):
     m,n=X.shape
     J=(-y*np.log(h(X,theta))-(1-y)*(np.log(1-h(X,theta)))).mean()
@@ -51,7 +41,7 @@ def getGradReg(theta,X,y,lam_par):
 
 
 def displayData(X,y):
-    n1,n2=5,5
+    n1,n2=5,10
     fig,ax=plt.subplots(n1,n2,sharex=True,sharey=True)
     img_num=0
     for i in range(n1):
@@ -60,85 +50,42 @@ def displayData(X,y):
             # You have to transpose to display correctly
             img=X[img_num,:].reshape(20,20).T
             ax[i][j].imshow(img,cmap='gray')
+            ax[i][j].set_title(str(y[img_num]))
+            ax[i][j].axis('off')
             img_num+=1
     return (fig,ax)
 
 
 
-def nnCostFunction(nn_params,\
-                   layer0_size,\
-                   layer1_size,\
-                   layer2_size,
-                   X,y,reg_param):
-    """
-    Computes loss using sum of square errors for a neural network
-    using theta as the parameter vector for linear regression to fit 
-    the data points in X and y with penalty reg_param.
-    """
-    m = len(y)    
-    # Reshape nn_params back into neural network
+def getNNCostReg(nn_params,\
+                 layer0_size,\
+                 layer1_size,\
+                 layer2_size,
+                 X,y,lam_par):
+    m=len(y)    # number of samples
     
+    # create a vector for each value of y
+    y_mat=np.zeros((m,layer2_size))
+    for i in range(m):
+        y_mat[i][y[i]]=1
+    # since the matlab version of thetas start enumiration from 1, not 0
+    y_mat=np.roll(y_mat,-1,axis=1)
+        
+    # Reshape nn_params back into neural network
     theta1=nn_params[:layer1_size*(layer0_size+1) ].reshape((layer1_size,layer0_size+1))
     theta2=nn_params[ layer1_size*(layer0_size+1):].reshape((layer2_size,layer1_size+1))
-    
-#    theta1 = nn_params[:(layer1_size * 
-#			           (layer0_size + 1))].reshape((layer1_size,layer0_size +1))
-#  
-#    theta2 = nn_params[-((layer1_size + 1) * 
-#                          layer2_size):].reshape((layer2_size,
-#					                             layer1_size + 1))
-   
-    # Turn scalar y values into a matrix of binary 
-    init_y = np.zeros((m,layer2_size)) # 5000 x 10
  
-    for i in range(m):
-        init_y[i][y[i]]=1
-
-    # Add column of ones to X
-    ones = np.ones((m,1)) 
-    d = np.hstack((ones,X))# add column of ones
- 
-    # forward propogation with theta1 and theta2
-    cost = [0]*m
-    # Initalize gradient vector
-    D1 = np.zeros_like(theta1)
-    D2 = np.zeros_like(theta2)
-    for i in range(m):
-	
-        a1 = d[i][:,None] # 401 x 1
-        z2 = np.dot(theta1,a1) # 25 x 1 
-        a2 = sigmoid(z2) # 25 x 1
-        a2 = np.vstack((np.ones(1),a2)) # 26 x 1
-        z3 = np.dot(theta2,a2) #10 x 1
-        h = sigmoid(z3) # 10 x 1
-        a3 = h # 10 x 1
-        cost[i] = (np.sum((-init_y[i][:,None])*(np.log(h)) -
-	              (1-init_y[i][:,None])*(np.log(1-h))))/m
-	
-        	# Calculate Gradient
-        d3 = a3 - init_y[i][:,None]
-        d2 = np.dot(theta2.T,d3)[1:]*(sigmoidGradient(z2))
-	
-        # Accumulate errors for gradient calculation
-        D1 = D1 + np.dot(d2,a1.T) # 25 x 401 (matches theta0)
-        D2 = D2 + np.dot(d3,a2.T) # 10 x 26 (matches theta1)
-
-    # regularization
-    reg = (reg_param/(2*m))*((np.sum(theta1[:,1:]**2)) + 
-	      (np.sum(theta2[:,1:]**2)))
+    # forward propagation
+    a1=np.c_[np.ones((X.shape[0],1)),X]
+    a2=h(a1,theta1.T)
+    a2=np.hstack((np.ones(a2.shape[0])[:,None],a2))
+    a3=h(a2,theta2.T)
     
-    # Compute final gradient with regularization
-    grad1 = (1.0/m)*D1 + (reg_param/m)*theta1
-    grad1[0] = grad1[0] - (reg_param/m)*theta1[0]
-    
-    grad2 = (1.0/m)*D2 + (reg_param/m)*theta2
-    grad2[0] = grad2[0] - (reg_param/m)*theta2[0]
-    
-    # Append and unroll gradient
-    grad = np.append(grad1,grad2).reshape(-1)
-    final_cost = sum(cost) + reg
+    # cost function
+    J=(-y_mat*np.log(a3)-(1-y_mat)*(np.log(1-a3))).sum()/m
+    J+=lam_par*((theta1[:,1:]**2).sum()+(theta2[:,1:]**2).sum())/2/m
 
-    return (final_cost, grad)
+    return J
 
 #%% PART I
 # load data from Matlab files
@@ -150,49 +97,50 @@ print('weights keys: ',weights.keys())
 #%% extract data
 
 y=np.asarray(data['y'],dtype='int').ravel()
-#y[y==10]=0
-y = (y - 1) % 10
+y[y==10]=0
 
 # Add constant for intercept
-X=np.c_[np.ones((data['X'].shape[0],1)),np.asarray(data['X'])]
-theta1_given=np.asarray(weights['Theta1'])
-theta2_given=np.asarray(weights['Theta2'])
+#X=np.c_[np.ones((data['X'].shape[0],1)),np.asarray(data['X'])]
+X=np.asarray(data['X'])
+theta1=np.asarray(weights['Theta1'])
+theta2=np.asarray(weights['Theta2'])
 
 print('Shapes of variables:')
-print('X: {} (with identity)'.format(X.shape))
+print('X: {} (without identity)'.format(X.shape))
 print('y: {}'.format(y.shape))
-print('Theta1: ',theta1_given.shape)
-print('Theta2: ',theta2_given.shape)
+print('Theta1: ',theta1.shape)
+print('Theta2: ',theta2.shape)
 print('\n')
 
 #%% visualize data
 
-sample=np.random.randint(0,X.shape[0],25)
-fig,ax=displayData(X[sample,1:],y[sample])
+sample=np.random.randint(0,X.shape[0],50)
+fig,ax=displayData(X[sample,:],y[sample])
 fig.show()
 
-#%%
-
+#%% test forward propagation via the cost function
 
 layer0_size=400
 layer1_size=25
 layer2_size=10
 
-# Unroll Parameters
-#nn_params = np.append(theta1_given,theta2_given).reshape(-1)
-nn_params=np.append(theta1_given.reshape([layer1_size*(layer0_size+1)]),\
-                    theta2_given.reshape([layer2_size*(layer1_size+1)]))
+nn_params=np.append(theta1.reshape([layer1_size*(layer0_size+1)]),\
+                    theta2.reshape([layer2_size*(layer1_size+1)]))
 
-print("Checking cost function without regularization...")
-reg_param = 0.0
-cost, g = nnCostFunction(nn_params,layer0_size,layer1_size,layer2_size,
-		                     X[:,1:],y,reg_param)
-
-# Test for correct cost
-np.testing.assert_almost_equal(0.287629,cost,decimal=6, err_msg="Cost incorrect.")
+print("Checking the cost function without regularization:")
+lam_par=0.0
+J=getNNCostReg(nn_params,layer0_size,layer1_size,layer2_size,X,y,lam_par)
+np.testing.assert_almost_equal(0.287629,J,decimal=6,\
+                               err_msg="Cost function is NOT correct")
+print('Cost function is correct')
 
 
-
+print("Checking the cost function with regularization:")
+lam_par=1.0
+J=getNNCostReg(nn_params,layer0_size,layer1_size,layer2_size,X,y,lam_par)
+np.testing.assert_almost_equal(0.383770,J,decimal=6,\
+                               err_msg="Cost function is NOT correct")
+print('Cost function is correct')
 
 
 
