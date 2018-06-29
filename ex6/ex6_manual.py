f5b15a7506cee3fd844695d789c650fd19f2faf6
Anton Buyskikh <anton.buyskikh@gmail.com>
2018-06-29 12:25:51 +0100

    Ex6: solve SVM problem w/ the gaussian kernel

--- ex6/ex6_manual.py
+++ ex6/ex6_manual.py
@@ -4,7 +4,8 @@
 Created on Fri Jun 29 10:08:23 2018
 
 @author: Anton Buyskikh
-@brief: Support Vector Machines.
+@brief: Support Vector Machines. Linear and Gaussian kernels.
+...
 """
 
 #%% libraries
@@ -25,13 +26,19 @@ def plotData(X,y):
     ax.plot(pos[:,0],pos[:,1],"k+",neg[:,0],neg[:,1],"yo")
     return (fig,ax)
 
+
+
+def getGaussianKernel(x1,x2,sigma):
+    # just an example of the kernel
+    return np.exp(-(np.linalg.norm(x1-x2).sum()**2)/2/sigma**2)
+
 #%% PART I
 # load data
 
-data=scipy.io.loadmat('data/ex6data1.mat')
-data.keys()
-X=data.get('X')
-y=data.get('y').ravel()
+data1=scipy.io.loadmat('data/ex6data1.mat')
+data1.keys()
+X=data1.get('X')
+y=data1.get('y').ravel()
 
 #%% visualize data
 
@@ -39,7 +46,7 @@ fig,ax=plotData(X,y)
 ax.legend(['Positive','Negative'])
 fig.show()
 
-#%% training linear SVM
+#%% training SVM with a linear boundary
 
 # C is the trade-off befween optimization and regularization of weights
 # try changing C (=1/lambda) form 10^-2 to 10^2
@@ -49,7 +56,7 @@ svm.fit(X,y)
 weights=svm.coef_[0]
 intercept=svm.intercept_[0]
 
-#%% draw the SVM boundary
+#%% draw the linear boundary
 
 xp=np.linspace(X.min(),X.max(),100)
 yp=-(weights[0]*xp+intercept)/weights[1]
@@ -59,9 +66,52 @@ ax.plot(xp,yp)
 ax.legend(['Positive','Negative','Boundary for C='+str(C)])
 fig.show()
 
-#%%
+#%% test the gaussian kernel
 
+x1=np.array([1,2, 1])
+x2=np.array([0,4,-1])
+sigma=2
+sim=getGaussianKernel(x1,x2,sigma)
 
+print('Gaussian Kernel between x1=[1,2,1], x2=[0,4,-1], sigma={0} : {1}'.format(sigma,sim))
+print('This value should be about 0.324652\n')
+
+#%% load more complex data
+
+data2=scipy.io.loadmat('data/ex6data2.mat')
+data2.keys()
+X=data2.get('X')
+y=data2.get('y').ravel()
+
+#%% visualize data
+
+fig,ax=plotData(X,y)
+ax.legend(['Positive','Negative'])
+fig.show()
+
+#%% training SVM with complex boundary
+
+# here we use the gaussian kernel (rbf)
+# C is the trade-off befween optimization and regularization of weights
+# sigma is the "sharpness" of the contribution of each datapoint to the coutur plot
+# try changing C and sigma
+C=10.0
+sigma=0.12
+svm=SVC(kernel='rbf',C=C,gamma=1/2.0/sigma**2)
+svm.fit(X,y)
+
+#%% draw the complex boundary
+
+x1=np.linspace(X[:,0].min(),X[:,0].max(),200)
+x2=np.linspace(X[:,1].min(),X[:,1].max(),200)
+x1,x2=np.meshgrid(x1,x2)
+yp=svm.predict(np.array([x1.ravel(),x2.ravel()]).T).reshape(x1.shape)
+
+fig,ax=plotData(X,y)
+plt.contour(x1,x2,yp)
+ax.legend(['Positive','Negative'])
+ax.set_title('C='+str(C)+', sigma='+str(sigma))
+fig.show()
 
 
 
