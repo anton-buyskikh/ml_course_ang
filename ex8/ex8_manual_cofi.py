fb17b9da2719bd7b76d7152599b48d406a3e8b98
Anton Buyskikh <anton.buyskikh@gmail.com>
2018-07-18 18:09:35 +0100

    Ex8: add and check cost&grad functions

--- ex8/ex8_manual_cofi.py
+++ ex8/ex8_manual_cofi.py
@@ -31,13 +31,44 @@ def reshapeParams(XTheta,nm,nu,nf):
 
 
 
-def cofiCostFunc(pars,Y,R,nu,nm,nf,lam_par=0.):
+def cofiCostFunc(pars,Y,R,nu,nm,nf,lam_par=0.0):
     X,Theta=reshapeParams(pars,nm,nu,nf)
     # const fuction
     cost=0.5*np.sum((X.dot(Theta.T)*R-Y)**2)
     # regularization
     cost+=(lam_par/2.)*np.sum(pars**2)    
     return cost
+
+
+
+def cofiGrad(pars,Y,R,nu,nm,nf,lam_par=0.0):
+    X,Theta=reshapeParams(pars,nm,nu,nf)
+    tmp=X.dot(Theta.T)*R-Y
+    # gradient
+    gradX=tmp.dot(Theta)
+    gradTheta=tmp.T.dot(X)
+    # regularization
+    gradX    +=lam_par*X
+    gradTheta+=lam_par*Theta
+    return flattenParams(gradX,gradTheta)
+
+
+
+def checkGradient(pars,Y,R,nu,nm,nf,lam_par=0.0):
+    print('Numerical Gradient \t cofiGrad \t\t Difference')
+    eps=0.001
+    npars=len(pars)
+    epsvec=np.zeros(npars)
+    grads=cofiGrad(pars,Y,R,nu,nm,nf,lam_par)
+    for i in range(10):
+        idx=np.random.randint(0,npars)
+        epsvec[idx]=eps
+        loss1=cofiCostFunc(pars-epsvec,Y,R,nu,nm,nf,lam_par)
+        loss2=cofiCostFunc(pars+epsvec,Y,R,nu,nm,nf,lam_par)
+        grad_num=(loss2-loss1)/(2*eps)
+        epsvec[idx]=0
+        print('%+0.15f \t %+0.15f \t %+0.15f'\
+              %(grad_num,grads[idx],grad_num-grads[idx]))
 
 #%% get data
 
@@ -77,13 +108,31 @@ nu=data2.get('num_users'   )[0,0]
 nm=data2.get('num_movies'  )[0,0]
 nf=data2.get('num_features')[0,0]
 
+## For now, reduce the data set size so that this runs faster
+nu=4
+nm=5
+nf=3
+X=X[:nm,:nf]
+Theta=Theta[:nu,:nf]
+Y=Y[:nm,:nu]
+R=R[:nm,:nu]
 
+#%% sanity check for the cost function
 
+# "...run your cost function. You should expect to see an output of 22.22."
+print('Cost with nu=4, nm=5, nf=3 is %0.2f.'\
+      %cofiCostFunc(flattenParams(X,Theta),Y,R,nu,nm,nf))
+    
+# "...with lambda=1.5 you should expect to see an output of 31.34."
+print('Cost with nu=4, nm=5, nf=3 (and lambda=1.5) is %0.2f.'\
+      %cofiCostFunc(flattenParams(X,Theta),Y,R,nu,nm,nf,lam_par=1.5))
 
+#%% sanity check for the gradient
 
-
-
-
+print("Checking gradient with lambda = 0:")
+checkGradient(flattenParams(X,Theta),Y,R,nu,nm,nf)
+print("\nChecking gradient with lambda = 1.5:")
+checkGradient(flattenParams(X,Theta),Y,R,nu,nm,nf,lam_par=1.5)
 
 
 
